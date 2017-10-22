87468693648f7cefd9220160a47dc9313cade86e
dElete0 <2219952901@qq.com>
2017-10-22 09:50:17 +0800

    a2

--- src/com/delete/ChatClient.java
+++ src/com/delete/ChatClient.java
@@ -4,6 +4,9 @@ import java.awt.*;
 
 public class ChatClient extends Frame{
 
+    TextField textField = new TextField();
+    TextArea textArea = new TextArea();
+
     public  static void main(String[] args){
         new ChatClient().launchFrame();
     }
@@ -11,6 +14,9 @@ public class ChatClient extends Frame{
     public void launchFrame(){
         setLocation(400,400);
         this.setSize(300,300);
+        add(textField,BorderLayout.SOUTH);
+        add(textArea,BorderLayout.NORTH);
+        pack();
         setVisible(true);
     }
 
