cfc9a6ffefa8a9851899eaa330d828b750ad77a7
dElete0 <2219952901@qq.com>
2017-10-22 10:55:41 +0800

    a4

--- src/com/delete/ChatClient.java
+++ src/com/delete/ChatClient.java
@@ -1,6 +1,8 @@
 package com.delete;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -28,9 +30,18 @@ public class ChatClient extends Frame{
             }
         });
 
+        //new 一个输出监听器
+        textField.addActionListener(new ChatClientListener());
+
         setVisible(true);
     }
 
-
+    private class ChatClientListener implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+            String s = textField.getText().trim();
+            textArea.setText(s);
+            textField.setText("");
+        }
+    }
 
 }
