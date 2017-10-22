363b2293b981a6cc22f91854e3f1839ef758c3fc
dElete0 <2219952901@qq.com>
2017-10-22 09:33:11 +0800

    a1

---
+++ src/com/delete/ChatClient.java
@@ -0,0 +1,17 @@
+package com.delete;
+
+import java.awt.*;
+
+public class ChatClient extends Frame{
+
+    public  static void main(String[] args){
+        new ChatClient().launchFrame();
+    }
+
+    public void launchFrame(){
+        setLocation(400,400);
+        this.setSize(300,300);
+        setVisible(true);
+    }
+
+}
